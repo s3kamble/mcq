e6557cb1f5cd7ade47544d9c8920baa6b852ae62
s3kamble <47882962+s3kamble@users.noreply.github.com>
2019-03-07 20:38:53 +0530

    Add files via upload

---
+++ mcqn.js
@@ -0,0 +1,49 @@
+$("#ans1_3").on("click",function()
+{
+	$('#crct1').css("background","lightgreen");
+});
+
+$("#ans2_2").on("click",function()
+{
+	$("#crct2").css("background","lightgreen");
+});
+
+$("#ans3_1").on("click",function()
+{
+	$("#crct3").css("background","lightgreen");
+});
+
+$("#ans4_1").on("click",function()
+{
+	$("#crct4").css("background","lightgreen");
+});
+
+$("#ans5_3").on("click",function()
+{
+	$("#crct5").css("background","lightgreen");
+});
+
+$("#ans6_1").on("click",function()
+{
+	$("#crct6").css("background","lightgreen");
+});
+
+$("#ans7_4").on("click",function()
+{
+	$("#crct7").css("background","lightgreen");
+});
+
+$("#ans8_1").on("click",function()
+{
+	$("#crct8").css("background","lightgreen");
+});
+
+$("#ans9_3").on("click",function()
+{
+	$("#crct9").css("background","lightgreen");
+});
+
+$("#ans10_4").on("click",function()
+{
+	$("#crct10").css("background","lightgreen");
+});
